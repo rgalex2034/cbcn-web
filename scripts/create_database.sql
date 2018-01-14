2b6b4401f6914adf62a5ea4bc39e1df858fecb88
Alex Ruiz <rgalex2034@hotmail.com>
2018-01-15 00:29:36 +0100

    Added creation database's creation script.

---
+++ scripts/create_database.sql
@@ -0,0 +1,49 @@
+DROP DATABASE IF EXISTS event_management;
+CREATE DATABASE event_management CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE event_management;
+
+CREATE TABLE place(
+    id INT AUTO_INCREMENT NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    latitude DOUBLE(7,6),
+    longitude DOUBLE(7,6),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE `group`(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    place INT,
+    description VARCHAR(500),
+    url_image VARCHAR(100),
+    contact_email VARCHAR(50),
+    district VARCHAR(50),
+    PRIMARY KEY(id),
+    FOREIGN KEY(place) REFERENCES place(id)
+);
+
+CREATE TABLE event(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    `date` DATETIME,
+    place INT,
+    url VARCHAR(100),
+    mail_contact VARCHAR(50),
+    `group` INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(`group`) REFERENCES `group`(id),
+    FOREIGN KEY(place) REFERENCES place(id)
+);
+
+CREATE TABLE `user`(
+    id INT AUTO_INCREMENT NOT NULL,
+    device_id VARCHAR(200) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE admin(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
