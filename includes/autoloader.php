0ced22ac2b913947ee241deefa777db029306cad
Alex Ruiz <rgalex2034@hotmail.com>
2018-01-15 20:29:43 +0100

    Added PauSabe\CBCN prefix for autoloader. Solved some bugs on config class.

--- includes/autoloader.php
+++ includes/autoloader.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * PSR-4 Compliant autoloader.
+ * Include files from the namespace prefix PauSabe\CBCN
+ */
+
 //Grab app path from parent file path.
 $file_dir = dirname(__FILE__);
 $root_app = dirname($file_dir);
@@ -8,10 +13,23 @@ $root_app = dirname($file_dir);
 set_include_path(get_include_path().PATH_SEPARATOR.$root_app);
 
 //Register autoload function that maps namespaces as directories.
-spl_autoload_register(function($clase){
-    $clase = str_replace("\\", DIRECTORY_SEPARATOR, $clase).".php";
-    if(is_file($clase)){
-        include $clase;
+spl_autoload_register(function($class) use($root_app){
+    echo "$class\n";
+    //Custom prefix for CBCN.
+    $prefix = "PauSabe\CBCN";
+
+    //If prefix is not found, return false.
+    if(strpos($class, $prefix) !== 0) return false;
+
+    //Delete prefix.
+    $class = str_replace($prefix, $root_app, $class);
+    echo "$class\n";
+
+    //Include file
+    $file = str_replace("\\", DIRECTORY_SEPARATOR, $class).".php";
+    echo "$file\n";
+    if(is_file($file)){
+        include $file;
         return true;
     }
     return false;
--- utils/Config.php
+++ utils/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace utils;
+namespace PauSabe\CBCN\utils;
 
 class Config{
 
@@ -14,7 +14,6 @@ class Config{
         $this->file = $filename;
     }
 
-
     /**
      * Return a config for a file.
      * If was previously loaded, return the same instance.
@@ -23,11 +22,12 @@ class Config{
      */
     public static function getConfig($filename){
         //Initate loaded configs as array if does not exists.
-        if(isset(self::$loaded_configs)) self::$loaded_configs = array();
+        if(!isset(self::$loaded_configs)) self::$loaded_configs = array();
         //Return loaded config if was previously saved on the array.
         if(isset(self::$loaded_configs[$filename])) return self::$loaded_configs[$filename];
         //Load config, save and return config.
         $config = new Config($filename);
+        $config->load();
         self::$loaded_configs[$filename] = $config;
         return $config;
     }
@@ -52,6 +52,9 @@ class Config{
 
     /**
      * Get the value of a config file
+     * @param string $param - Config value to grab
+     * @param string $section - Section where the value is
+     * @return mixed - Any value stored in the config file
      */
     public function getValue($param, $section = null){
         //Throw exception if no section specified when it has loaded sections.
